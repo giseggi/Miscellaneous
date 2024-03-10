ad0b7ae79b4de61b3b84ebb84e6a8c6ad601221e
kylee <giseggi57@gmail.com>
2024-03-10 15:25:44 +0900

    Add label

--- src/main/java/com/gsg/miscellaneous/MiscellaneousApplication.java
+++ src/main/java/com/gsg/miscellaneous/MiscellaneousApplication.java
@@ -13,28 +13,28 @@ import java.io.IOException;
 public class MiscellaneousApplication {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-//        JvmMetrics.builder().register(); // initialize the out-of-the-box JVM metrics
-//
-//        Gauge gauge = Gauge.builder()
-//                .name("my_gauge_test")
-//                .help("example counter")
-//                .labelNames("status")
-//                .register();
-//
-//        gauge.labelValues("ok").inc();
-//        gauge.labelValues("ng").inc();
-//        gauge.labelValues("ng").inc();
-//        gauge.labelValues("ok").inc();
-//        gauge.labelValues("ok").inc();
-//        gauge.labelValues("error").inc();
-//
-//        HTTPServer server = HTTPServer.builder()
-//                .port(9400)
-//                .buildAndStart();
-//
-//        System.out.println("HTTPServer listening on port http://localhost:" + server.getPort() + "/metrics");
-//
-//        Thread.currentThread().join(); // sleep forever
+        JvmMetrics.builder().register(); // initialize the out-of-the-box JVM metrics
+
+        Gauge gauge = Gauge.builder()
+                .name("my_gauge_test")
+                .help("example counter")
+                .labelNames("status", "type")
+                .register();
+
+        gauge.labelValues("ok", "A").inc();
+        gauge.labelValues("ng", "B").inc();
+        gauge.labelValues("ng", "O").inc();
+        gauge.labelValues("ok", "AB").inc();
+        gauge.labelValues("ok", "A").inc();
+        gauge.labelValues("error", "B").inc();
+
+        HTTPServer server = HTTPServer.builder()
+                .port(9400)
+                .buildAndStart();
+
+        System.out.println("HTTPServer listening on port http://localhost:" + server.getPort() + "/metrics");
+
+        Thread.currentThread().join(); // sleep forever
         SpringApplication.run(MiscellaneousApplication.class, args);
     }
 
